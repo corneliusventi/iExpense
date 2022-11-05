458bb8932065dfd355ca6ddf9751fb3b1bd62cb5
Cornelius Venti <corneliusventi@gmail.com>
2022-11-05 21:53:14 +0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Cornelius Venti on 05/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
